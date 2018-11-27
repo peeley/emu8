00038a4c0cf52d7b0883a412b5367ab2720fd8c2
Noah Snelson <noah.snelson@protonmail.com>
2018-11-27 00:44:23 -0900

    SDL now working - graphics and input buggy.

--- chip8.cpp
+++ chip8.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include "SDL2/SDL.h"
 using std::cout;
 using std::endl;
 using std::size_t;
@@ -71,7 +72,6 @@ bool Chip8::load(const char * filename){
 int idx = 0;
 void Chip8::cycle(){
 	opcode = memory[pc] << 8 | memory[pc+1]; // Each opcode is 8 bit
-	cout << std::hex << opcode << endl;
 	++idx;
 	switch(opcode & 0xF000){	// Parse current opcode
 		
@@ -267,7 +267,7 @@ void Chip8::cycle(){
 			for(int y = 0; y < height; y++){
 				px = memory[I + y];
 				for(int x = 0; x < 8; x++){
-					if((px & (0x80 >> x) != 0)){
+					if((px & (0x80 >> x)) != 0){
 						int pixelLoc = v[xReg] + x + ((v[yReg] + y) * 64);
 						if(graphics[pixelLoc] == 1)
 							v[0xF] = 1;
@@ -398,11 +398,68 @@ int main(int argc, char * argv[]){
 		cout << "Please provide ROM filename." << endl;
 		exit(1);
 	}
+	int windowWidth =  1024;
+	int windowHeight = 512;
 	cout << "Loading ROM file: " << argv[1] << endl;
 	chip.load(argv[1]);
 	cout << "Finished loading ROM." << endl;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window * window = SDL_CreateWindow("emu8", 50, 50, windowWidth, windowHeight, 0);
+	SDL_Renderer * render = SDL_CreateRenderer(window, -1, 0);
+	SDL_RenderSetLogicalSize(render, windowWidth, windowHeight);
+	SDL_Texture * texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_ARGB8888,
+											SDL_TEXTUREACCESS_STREAMING , 64, 32);
+
+	unsigned int graphicsBuffer[2048];
+	unsigned char pixel;
+	unsigned char keybindings[16] = {
+		SDLK_x,	SDLK_1,	SDLK_2,
+		SDLK_3,	SDLK_q,	SDLK_w,
+		SDLK_e,	SDLK_a,	SDLK_s,
+		SDLK_d,	SDLK_z,	SDLK_c,
+		SDLK_4,	SDLK_r,	SDLK_f,
+		SDLK_v
+	};
 	while(true){
 		chip.cycle();
+		SDL_Event event;
+		while(SDL_PollEvent(&event)){
+			if(event.type == SDL_QUIT){
+				SDL_DestroyWindow(window);
+				SDL_Quit();
+				exit(0);
+			}
+			if(event.type == SDL_KEYDOWN){
+				if (event.key.keysym.sym == SDLK_ESCAPE){
+					SDL_DestroyWindow(window);
+					SDL_Quit();
+					exit(0);
+				}
+				for(int i = 0; i < 16; i++){
+					if(event.key.keysym.sym == keybindings[i]){
+						chip.input[i] = 1;
+					}
+				}
+			}
+			if(event.type == SDL_KEYUP) {
+                for (int i = 0; i < 16; i++) {
+                    if (event.key.keysym.sym == keybindings[i]) {
+                        chip.input[i] = 0;
+                    }
+                }
+			}
+		}
+		if(chip.draw){
+			chip.draw = false;
+			for(int i = 0; i < 2048; i++){
+				pixel = chip.graphics[i];
+				graphicsBuffer[i] = (0x00FFFFFF * pixel) | 0xFF000000;
+			}
+			SDL_UpdateTexture(texture, NULL, graphicsBuffer, 64 * sizeof(unsigned int));
+			SDL_RenderClear(render); 
+			SDL_RenderCopy(render, texture, NULL, NULL);
+			SDL_RenderPresent(render);
+		}
 		std::this_thread::sleep_for(std::chrono::microseconds(1200));		
 	}
 }
